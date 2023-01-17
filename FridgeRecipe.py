3fe4daede7d881d19d47ffa46d045b6ca44f96c2
Asimiea Sobomate-Victor <asimieasobomate@gmail.com>
2023-01-17 15:58:19 -0400

    updated

--- FridgeRecipe.py
+++ FridgeRecipe.py
@@ -1,127 +1,48 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter.messagebox import showerror
+import requests
 
+def get_recipes(ingredients, vegetarian=False):
+    # Make API call to a recipe website using the ingredients
+    url = 'https://www.recipepuppy.com/api/?i=' + ingredients
+    if vegetarian:
+        url += '&q=vegetarian'
+    response = requests.get(url)
+    data = response.json()
 
-class TemperatureConverter:#this should be changed to search class
-    @staticmethod
-    def fahrenheit_to_celsius(f, format=True):
-        result = (f - 32) * 5/9
-        if format:
-            return f'{f} Fahrenheit = {result:.2f} Celsius'
-        return result
+    # Extract the list of recipes from the API response
+    recipes = data['results']
 
-    @staticmethod
-    def celsius_to_fahrenheit(c, format=True):
-        result = c * 9/5 + 32
-        if format:
-            return f'{c} Celsius = {result:.2f} Fahrenheit'
-        return result
+    # Return the list of recipes
+    return recipes
 
+def on_submit():
+    ingredients = entry.get()
+    recipe_list.delete(0,tk.END)
+    if vegetarian_var.get() == 1:
+        recipes = get_recipes(ingredients, vegetarian=True)
+    else:
+        recipes = get_recipes(ingredients)
+    for recipe in recipes:
+        recipe_list.insert(tk.END, recipe['title'])
 
-class ConverterFrame(ttk.Frame):
-    def __init__(self, container, type, converter):
-        super().__init__(container)
+root = tk.Tk()
+root.title("Recipe Finder")
 
-        self.type = type
-        self.converter = converter
+label = ttk.Label(root, text="Enter ingredients separated by commas:")
+label.pack()
 
-        # field options
-        options = {'padx': 5, 'pady': 0}
+entry = ttk.Entry(root)
+entry.pack()
 
-        # temperature label
-        self.ingredientsLabel = ttk.Label(self, text=self.type)
-        self.ingredientsLabel.grid(column=0, row=0, sticky='w',  **options)
+vegetarian_var = tk.IntVar()
+vegetarian_check = tk.Checkbutton(root, text="Vegetarian", variable=vegetarian_var)
+vegetarian_check.pack()
 
-        # temperature entry
-        self.temperature = tk.StringVar()
-        self.temperature_entry = ttk.Entry(self, textvariable=self.temperature)
-        self.temperature_entry.grid(column=1, row=0, sticky='w', **options)
-        self.temperature_entry.focus()
+submit_button = ttk.Button(root, text="Submit", command=on_submit)
+submit_button.pack()
 
-        # button
-        self.searchButton = ttk.Button(self, text='Search')
-        self.searchButton.grid(column=2, row=0, sticky='w', **options)
-        self.searchButton.configure(command=self.convert)
+recipe_list = tk.Listbox(root)
+recipe_list.pack()
 
-        # result label
-        self.result_label = ttk.Label(self)
-        self.result_label.grid(row=1, columnspan=7, **options)
-
-        # add padding to the frame and show it
-        self.grid(column=0, row=0, padx=5, pady=5, sticky="nsew")
-
-    def convert(self, event=None):
-        """  Handle button click event
-        """
-        try:
-            input_value = float(self.temperature.get())
-            result = self.converter(input_value)
-            self.result_label.config(text=result)
-        except ValueError as error:
-            showerror(title='Error', message=error)
-
-    def reset(self):
-        self.temperature_entry.delete(0, "end")
-        self.result_label.text = ''
-
-
-class ControlFrame(ttk.LabelFrame):
-    def __init__(self, container):
-
-        super().__init__(container)
-        self['text'] = 'Options'
-
-        # radio buttons
-        self.selected_value = tk.IntVar()
-
-        ttk.Radiobutton(
-            self,
-            text='Non-Vegetarian',
-            value=0,
-            variable=self.selected_value,
-            command=self.change_frame).grid(column=0, row=0, padx=5, pady=5)
-
-        ttk.Radiobutton(
-            self,
-            text='Vegetarian',
-            value=1,
-            variable=self.selected_value,
-            command=self.change_frame).grid(column=1, row=0, padx=5, pady=5)
-
-        self.grid(column=0, row=1, padx=5, pady=5, sticky='ew')
-
-        # initialize frames
-        self.frames = {}
-        self.frames[0] = ConverterFrame(
-            container,
-            'Enter Ingredients',
-            TemperatureConverter.fahrenheit_to_celsius
-            )
-        self.frames[1] = ConverterFrame(
-            container,
-            'Enter Ingredients',
-            TemperatureConverter.celsius_to_fahrenheit
-            )
-
-        self.change_frame()
-
-    def change_frame(self):
-        frame = self.frames[self.selected_value.get()]
-        frame.reset()
-        frame.tkraise()
-
-
-class App(tk.Tk):
-    def __init__(self):
-        super().__init__()
-
-        self.title('Fridge Recipe')
-        #self.geometry('350x120')
-        self.resizable(False, False)
-
-
-if __name__ == "__main__":
-    app = App()
-    ControlFrame(app)
-    app.mainloop()
+root.mainloop()
